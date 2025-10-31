54bb37cb4519b460798dd5474485c5f8cb9630b0
Leeka99 <dsrn0000@gmail.com>
2025-10-31 14:11:40 +0900

    refactor: 사용자 입력 클래스 분리

---
+++ src/main/java/lotto/view/input/Input.java
@@ -0,0 +1,11 @@
+package lotto.view.input;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+
+    public static String read() {
+        return Console.readLine();
+    }
+
+}
