3bbb781be7f776808d08837f0c6536477620ba80
Leeka99 <dsrn0000@gmail.com>
2025-10-31 11:38:10 +0900

    style: 보너스 번호 예외처리 문구 수정

--- src/main/java/lotto/util/exception/message/BonusExceptionMessage.java
+++ src/main/java/lotto/util/exception/message/BonusExceptionMessage.java
@@ -5,7 +5,7 @@ public enum BonusExceptionMessage {
     NOT_DIGIT("[ERROR] 숫자 1개만 입력해주세요."),
     NOT_ZERO("[ERROR] 0 입력 불가. 1 ~ 45 사이의 숫자 1개만 입력해주세요."),
     OVER_LOTTO_NUMBER("[ERROR] 45 초과 입력 불가. 1 ~ 45 사이의 숫자 1개만 입력해주세요."),
-    NOT_DUPLICATE_NUMBER("[ERROR] 1 ~ 45 사이의 로또 번호와 중복되지 않는 값 1개를 입력해주세요.");
+    NOT_DUPLICATE_NUMBER("[ERROR] 1 ~ 45 사이의 당첨 번호와 중복되지 않는 값 1개를 입력해주세요.");
 
     private final String message;
 
